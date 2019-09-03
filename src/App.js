30ab14a039fbe91fd2bb40f4642af06e679570a6
allisonkydy <allisnelly@gmail.com>
2019-09-02 21:49:07 -0500

    housekeeping

--- src/App.js
+++ src/App.js
@@ -6,14 +6,10 @@ import TodoForm from "./components/TodoComponents/TodoForm";
 import "./components/TodoComponents/Todo.css";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
     this.state = {
-      todoData: [],
-      newTodo: {}
+      todoData: []
     };
   }
 
@@ -36,27 +32,30 @@ class App extends React.Component {
         // toggle completed property at current index
         todoData[index].completed = !this.state.todoData[index].completed;
         // update state with changed temp copy
-        this.setState({ todoData: todoData })
-      } 
-    })
+        this.setState({ todoData: todoData });
+      }
+    });
   };
 
-  // TODO remove todo functionality
+  // remove completed todos
   removeTodo = () => {
     // make a temp copy
     let todoData = [...this.state.todoData];
     // filter for incomplete tasks
     const incomplete = todoData.filter(todo => todo.completed === false);
     // update state with copy
-    this.setState({ todoData: incomplete})
-  }
+    this.setState({ todoData: incomplete });
+  };
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoList todoData={this.state.todoData} toggleComplete={this.toggleComplete}/>
-        <TodoForm addTodo={this.addTodo} removeTodo={this.removeTodo}/>
+        <TodoList
+          todoData={this.state.todoData}
+          toggleComplete={this.toggleComplete}
+        />
+        <TodoForm addTodo={this.addTodo} removeTodo={this.removeTodo} />
       </div>
     );
   }
--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function Todo(props) {
-   const { task, toggleComplete, id, completed } = props;
-   return (
-      <div onClick={() => toggleComplete(id)} className={completed ? "complete" : "incomplete"}>{task}</div>
-   )
+  const { task, toggleComplete, id, completed } = props;
+  return (
+    <div
+      onClick={() => toggleComplete(id)}
+      className={completed ? "complete" : "incomplete"}
+    >
+      {task}
+    </div>
+  );
 }
 
 export default Todo;
